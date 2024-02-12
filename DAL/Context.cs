a2b3dad1c82372de51eff0afc518a0cc9adc14ae
Ruth2002Casilla <ruth.b2002casillagarcia@gmail.com>
2024-02-12 18:42:47 -0400

    Context de Metas completado

---
+++ DAL/Context.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Parcial1_AP1_RuthCasillaGarcia.Models;
+
+namespace Parcial1_AP1_RuthCasillaGarcia.DAL
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context>options) : base(options) { }
+        public DbSet<Metas> Metas { get; set; }
+    }
+}
